8a7ce825c1cbd6e5591ba46135e34ba33272f638
pbrunet44 <46230748+pbrunet44@users.noreply.github.com>
2019-10-14 14:37:57 -0700

    Added rounded buttons, submit button links to blank activity

--- app/src/main/java/com/example/yabeeprototype/MainActivity.java
+++ app/src/main/java/com/example/yabeeprototype/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.yabeeprototype;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, blank_activity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
---
+++ app/src/main/java/com/example/yabeeprototype/blank_activity.java
@@ -0,0 +1,14 @@
+package com.example.yabeeprototype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class blank_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.blank_activity);
+    }
+}
