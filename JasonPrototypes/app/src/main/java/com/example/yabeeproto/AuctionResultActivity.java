b7fdc5d17f1ea4676684d29c627b0fc4b3588066
Jason <jason.morgan460@myci.csuci.edu>
2019-10-15 11:30:28 -0700

    Created My Prototypes
    created buying list activity, selling list activity and auction result activity.

---
+++ JasonPrototypes/app/src/main/java/com/example/yabeeproto/AuctionResultActivity.java
@@ -0,0 +1,15 @@
+package com.example.yabeeproto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuctionResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auction_result);
+    }
+
+}
